3008e6aa1618b745132bdedc1c576751d8fd3f37
laozhao <zlaozhaozaici@gmail.com>
2018-02-07 16:24:50 +0800

    没有id111111了

--- src/main/java/com/lanou/entity/BizCheckResult.java
+++ src/main/java/com/lanou/entity/BizCheckResult.java
@@ -20,7 +20,7 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "biz_check_result", catalog = "oa")
 public class BizCheckResult implements java.io.Serializable {
-	//id111111
+	
 	private Integer id;
 	private BizClaimVoucher bizClaimVoucher;
 	private SysEmployee sysEmployee;  
